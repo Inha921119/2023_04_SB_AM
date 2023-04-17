1dff6e316cc7df8e03c030e006d92e1b3379ed74
Inha921119 <dlsgk921119@gmail.com>
2023-04-17 15:56:36 +0900

    Member join기능 구현

---
+++ src/main/java/com/koreaIT/demo/controller/UsrMemberController.java
@@ -0,0 +1,45 @@
+package com.koreaIT.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.koreaIT.demo.service.MemberService;
+import com.koreaIT.demo.vo.Member;
+
+@Controller
+public class UsrMemberController {
+	
+	private MemberService memberService;
+	
+	@Autowired
+	public UsrMemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+	
+	// 액션 메서드
+	@RequestMapping("/usr/member/doJoin")
+	@ResponseBody
+	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+		
+		memberService.joinMember(loginId, loginPw, name, nickname, cellphoneNum, email);
+		
+		int id = memberService.getLastInsertId();
+		
+		return memberService.getMemberById(id);
+	}
+	
+	@RequestMapping("/usr/member/getMember")
+	@ResponseBody
+	public Object getMember(int id) {
+		
+		Member member = memberService.getMemberById(id);
+		
+		if(member == null) {
+			return id + "번 회원은 존재하지 않습니다.";
+		}
+		
+		return member;
+	}
+}
