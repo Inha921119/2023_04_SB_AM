82478356cef7ae8ab0d25cc92383a122f4fdfe03
Inha921119 <dlsgk921119@gmail.com>
2023-04-18 14:22:21 +0900

    loginId dupplicate check, 유효성 검사

--- src/main/java/com/koreaIT/demo/service/MemberService.java
+++ src/main/java/com/koreaIT/demo/service/MemberService.java
@@ -16,12 +16,21 @@ public class MemberService {
 		this.memberRepository = memberRepository;
 	}
 	
-	public void doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+	public int doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+		
+		Member existMember = getMemberByLoginId(loginId);
+		
+		if (existMember != null) {
+			return -1;
+		}
+		
 		memberRepository.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
+		
+		return memberRepository.getLastInsertId();
 	}
 	
-	public int getLastInsertId() {
-		return memberRepository.getLastInsertId();
+	public Member getMemberByLoginId(String loginId) {
+		return memberRepository.getMemberByLoginId(loginId);
 	}
 	
 	public Member getMemberById(int id) {
